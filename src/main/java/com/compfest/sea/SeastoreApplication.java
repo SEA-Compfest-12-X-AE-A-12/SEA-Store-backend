fe2364bc5fcedec6073f1c5b9ff44ee9b502286f
addffa <adli.daffa5@gmail.com>
2020-08-27 22:52:42 +0700

    init commit

---
+++ src/main/java/com/compfest/sea/SeastoreApplication.java
@@ -0,0 +1,13 @@
+package com.compfest.sea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeastoreApplication.class, args);
+	}
+
+}
