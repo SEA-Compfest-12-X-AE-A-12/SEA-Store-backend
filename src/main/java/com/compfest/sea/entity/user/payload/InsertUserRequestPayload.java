dc451bba46cd9b771e776813f543617dafe2a222
Alvin Febriando <alvinfebriando@outlook.com>
2020-09-02 16:17:33 +0700

    Delete redundant getter and setter annotation

--- src/main/java/com/compfest/sea/entity/user/payload/InsertUserRequestPayload.java
+++ src/main/java/com/compfest/sea/entity/user/payload/InsertUserRequestPayload.java
@@ -3,12 +3,8 @@ package com.compfest.sea.entity.user.payload;
 import com.compfest.sea.entity.user.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 @AllArgsConstructor
 public class InsertUserRequestPayload {
     private String name;
