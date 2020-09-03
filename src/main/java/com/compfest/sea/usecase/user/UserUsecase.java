0ba418336efc34b7260d6c7763d02e77a6acde72
addffa <adli.daffa5@gmail.com>
2020-09-03 10:23:46 +0700

    Add login feature

--- src/main/java/com/compfest/sea/usecase/user/UserUsecase.java
+++ src/main/java/com/compfest/sea/usecase/user/UserUsecase.java
@@ -2,10 +2,11 @@ package com.compfest.sea.usecase.user;
 
 import java.util.List;
 import com.compfest.sea.entity.user.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface UserUsecase {
+public interface UserUsecase extends UserDetailsService {
     public List<User> getAllUser();
 
     public User findUserById(int id);
@@ -17,4 +18,6 @@ public interface UserUsecase {
     public User updateProfile(int id, User updatedUser);
 
     public void deleteUser(int id);
+
+    public String authenticate(String email, String password);
 }
