fdcf38d4be47f371e2b18d9d3e98bf7a4bddcd18
Alvin Febriando <alvinfebriando@outlook.com>
2020-09-04 12:54:00 +0700

    Throw some exception

--- src/main/java/com/compfest/sea/delivery/user/UserDelivery.java
+++ src/main/java/com/compfest/sea/delivery/user/UserDelivery.java
@@ -5,10 +5,11 @@ import com.compfest.sea.entity.user.model.User;
 import com.compfest.sea.entity.user.payload.InsertUserRequestPayload;
 import com.compfest.sea.entity.user.payload.LoginRequestPayload;
 import com.compfest.sea.entity.user.payload.LoginResponse;
+import com.compfest.sea.exception.ResourceAlreadyExistException;
+import com.compfest.sea.exception.ResourceNotFoundException;
 import com.compfest.sea.usecase.user.UserUsecase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,26 +38,43 @@ public class UserDelivery {
     @PostMapping
     public User register(@RequestBody InsertUserRequestPayload payload) {
         User convertedFromPayload = UserAdapter.convertInsertUserRequestPayloadToUser(payload);
+        User user = userUsecase.findUserByEmail(convertedFromPayload.getEmail());
+        if (user != null) {
+            throw new ResourceAlreadyExistException("user");
+        }
         return userUsecase.createUser(convertedFromPayload);
     }
 
     @GetMapping("{id}")
     public User getUserById(@PathVariable("id") int id) {
-        return userUsecase.findUserById(id);
+        User user = userUsecase.findUserById(id);
+        if (user == null) {
+            throw new ResourceNotFoundException("user");
+        }
+        return user;
     }
 
     @PutMapping("{id}")
     public User updateProfile(@PathVariable("id") int id, @RequestBody User updatedUser) {
+        User user = userUsecase.findUserById(id);
+        if (user == null) {
+            throw new ResourceNotFoundException("user");
+        }
         return userUsecase.updateProfile(id, updatedUser);
     }
 
     @DeleteMapping("{id}")
     public void deleteUser(@PathVariable("id") int id) {
+        User user = userUsecase.findUserById(id);
+        if (user == null) {
+            throw new ResourceNotFoundException("user");
+        }
         userUsecase.deleteUser(id);
     }
 
     @PostMapping("/login")
     public LoginResponse authenticateUser(@RequestBody LoginRequestPayload payload) {
-        return new LoginResponse(userUsecase.authenticate(payload.getEmail(), payload.getPassword()));
+        return new LoginResponse(
+                userUsecase.authenticate(payload.getEmail(), payload.getPassword()));
     }
 }
