5db9db45cb00fc3f172fb12676749720ed5e0969
Muhammad Jilham Luthfi <mjilhamluthfi150500@gmail.com>
2020-09-07 14:09:27 +0700

    feat(admin): add admin request payload model

---
+++ src/main/java/com/compfest/sea/entity/admin/payload/InsertAdminRequestPayload.java
@@ -0,0 +1,17 @@
+package com.compfest.sea.entity.admin.payload;
+
+import com.compfest.sea.entity.user.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class InsertAdminRequestPayload {
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+    private Role role;
+    private String token;
+}
