4ac4a6db342de99d59933bd2bff14a22d269b8db
Muhammad Jilham Luthfi <mjilhamluthfi150500@gmail.com>
2020-09-10 20:02:28 +0700

    feat(admin): add admin usecase for registering admin

---
+++ src/main/java/com/compfest/sea/usecase/admin/AdminUsecaseImpl.java
@@ -0,0 +1,34 @@
+package com.compfest.sea.usecase.admin;
+
+import com.compfest.sea.entity.user.model.User;
+import com.compfest.sea.exception.InvalidAdminTokenException;
+import com.compfest.sea.usecase.user.UserUsecase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service("AdminUsecase")
+class AdminUsecaseImpl implements AdminUsecase {
+    private UserUsecase userUsecase;
+
+    @Autowired
+    public AdminUsecaseImpl(@Qualifier("UserUsecaseImpl") UserUsecase userUsecase) {
+        this.userUsecase = userUsecase;
+    }
+
+    @Override
+    public User registerAdmin(User user) {
+        User createdUser = userUsecase.createUser(user);
+        return createdUser;
+    }
+
+    @Override
+    public void acceptTransaction() {
+        //TODO
+    }
+
+    @Override
+    public void rejectTransaction() {
+        //TODO
+    }
+}
