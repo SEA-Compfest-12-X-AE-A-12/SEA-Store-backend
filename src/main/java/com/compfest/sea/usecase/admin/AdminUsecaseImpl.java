08ddff90dc921b1c5a1ce025d6da6adafb76daba
Muhammad Jilham Luthfi <mjilhamluthfi150500@gmail.com>
2020-09-11 15:06:46 +0700

    rename admin service

--- src/main/java/com/compfest/sea/usecase/admin/AdminUsecaseImpl.java
+++ src/main/java/com/compfest/sea/usecase/admin/AdminUsecaseImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service("AdminUsecase")
+@Service("AdminUsecaseImpl")
 class AdminUsecaseImpl implements AdminUsecase {
     private UserUsecase userUsecase;
 
